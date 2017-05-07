fc752a71bcaad919fd8465a00416adad673eb1cf
Conor Dockry <conordockry@gmail.com>
2017-05-07 12:10:32 -0400

    fix tabs

--- index.js
+++ index.js
@@ -1,50 +1,48 @@
 'use strict';
 module.exports = {
-
-	"parser": "babel-eslint",
-	    "extends": "xo-react",
-	    "envs": [
-				      "node",
-				      "browser",
-				      "es6"
-				    ],
-	    "plugins": [
-				      "class-property"
-				    ],
-	    "semicolon": false,
-	    "rules": {
-				      "no-var": "error",
-				      "prefer-const": "error",
-				      "capitalized-comments": "off",
-				      "object-curly-spacing": [
-								        "error",
-								        "always"
-								      ],
-				      "comma-dangle": [
-								        "error",
-								        "always-multiline"
-								      ],
-				      "operator-linebreak": [
-								        "error",
-								        "after",
-								        {
-													          "overrides": {
-																			            "?": "before",
-																			            ":": "before"
-																			          }
-													        }
-								      ],
-				      "jsx-quotes": [
-								        "error",
-								        "prefer-single"
-								      ],
-				      "react/jsx-closing-bracket-location": [
-								        1,
-								        {
-													          "selfClosing": "after-props",
-													          "nonEmpty": "after-props"
-													        }
-								      ]
-				    }
-
+	parser: 'babel-eslint',
+	extends: 'xo-react',
+	envs: [
+		'node',
+		'browser',
+		'es6'
+	],
+	plugins: [
+		'class-property'
+	],
+	semicolon: false,
+	rules: {
+		'no-var': 'error',
+		'prefer-const': 'error',
+		'capitalized-comments': 'off',
+		'object-curly-spacing': [
+			'error',
+			'always'
+		],
+		'comma-dangle': [
+			'error',
+			'always-multiline'
+		],
+		'operator-linebreak': [
+			'error',
+			'after',
+			{
+				overrides: {
+					'?': 'before',
+					':': 'before'
+				}
+			}
+		],
+		'jsx-quotes': [
+			'error',
+			'prefer-single'
+		],
+		'react/jsx-closing-bracket-location': [
+			1,
+			{
+				selfClosing: 'after-props',
+				nonEmpty: 'after-props'
+			}
+		]
+	}
 }
